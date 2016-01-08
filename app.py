c961d4a4088e5ba9139f89cc7d301067d6e63675
anzolo <alzolotov@gmail.com>
2016-01-09 00:00:21 +0500

    test

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import bottle
 
 from bottle import route, template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.remontas24
+
+@route('/static/<filename:path>')
+def static(filename):
+    '''
+    Serve static files
+    '''
+    return bottle.static_file(filename, root='./static')
 
 @route('/')# Handle HTTP GET for the application root
 def index():
-    return template('\n<h1>{{message}}</h1>', message='Привет, Remontas24!!!')
+    return template('\n<h1>{{message}}</h1>', message='Главная страница нового Remontas24!')
+
+@route('/adminka')# Handle HTTP GET for the application root
+def adminka():
+    return template('adminka')
+
 
 # Run bottle internal test server when invoked directly ie: non-uxsgi mode
 if __name__ == '__main__':
