45d040fa3679f8f600bb38983b5b9e7b29099bd9
anzolo <alzolotov@gmail.com>
2016-02-28 01:12:57 +0800

    изменил адрес локального сервера

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import remontas
 
 # Run bottle internal test server when invoked directly ie: non-uxsgi mode
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080)
+    run(host='127.0.0.1', port=8080)
 # Run bottle in application mode. Required in order to get the application working with uWSGI!
 else:
     app = application = default_app()
