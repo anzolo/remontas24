ccc01b66c88b68fc7f2773ede69a0fddf38da41f
Кришталев Александр <AGKrishtalev@gmail.com>
2016-02-27 18:37:51 +0500

    Поправили ссылки на сервисы

--- adminka/public/app/backend.js
+++ adminka/public/app/backend.js
@@ -2,5 +2,5 @@ var remBackend = angular.module('remBackend', ['ngResource']);
 
 remBackend.factory('masters', ['$resource', 'CONFIG',
   function ($resource, CONFIG) {
-        return $resource('http://' + CONFIG.app_url + '/api/adminka/masters/:id');
+        return $resource('/api/adminka/masters/:id');
                 }]);
--- remontas/public/angular/services.js
+++ remontas/public/angular/services.js
@@ -2,5 +2,5 @@ var remBackend = angular.module('remBackend', ['ngResource']);
 
 remBackend.factory('searchMasters', ['$resource',
   function ($resource) {
-        return $resource('http://0.0.0.0:8080/api/main/searchMasters');
+        return $resource('/api/main/searchMasters');
                 }]);
