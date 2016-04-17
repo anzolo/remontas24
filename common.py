c57660983481a49e2ff2af302a14d65d21f7b74b
anzolo <alzolotov@gmail.com>
2016-04-17 17:26:01 +0500

    удалил неиспользуемый код

--- common.py
+++ common.py
@@ -6,13 +6,6 @@ import os
 import uuid
 
 from PIL import Image, ImageEnhance
-from PIL import ImageFilter
-from PIL import ImageOps
-from PIL import ImageDraw
-from PIL import ImageFont
-
-from wand.image import Image as WandImage
-import subprocess
 
 #  хранилище фото
 @route('/storage/<filename:path>')
@@ -105,59 +98,3 @@ def watermarkPhoto(photo, watermark, position, opacity=1):
     # composite the watermark with the layer
     photoWithWatermark = Image.composite(layer, im, layer)
     photoWithWatermark.save(photo,'PNG')
-
-def stampWaterMarkOnPhoto(filename):
-    pass
-    # imageW = 0
-    # imageH = 0
-    # img_filename = "%s/../data/tarte_fruits.jpg" % demo_dir
-    # font_filename = "%s/timR24.pil" % demo_dir
-    # im = None
-    # ft = ImageFont.load(font_filename)
-    #
-    # im0 = Image.open(img_filename)
-    # imageW = im0.size[0]
-    # imageH = im0.size[1]
-    #
-    # im = ImageOps.solarize(im0, 128)
-    #
-    # draw = ImageDraw.Draw(im)
-    # draw.line((0, 0) + im.size, fill=(255, 255, 255))
-    # draw.line((0, im.size[1], im.size[0], 0), fill=(255, 255, 255))
-    # wh = ft.getsize("GLSL Hacker")
-    # draw.text((im.size[0] / 2 - wh[0] / 2, im.size[1] / 2 + 20), "GLSL Hacker",
-    #           fill=(255, 255, 0), font=ft)
-    # draw.text((im.size[0] / 2 - wh[0] / 2, im.size[1] / 2 - 60), "GLSL Hacker",
-    #           fill=(255, 255, 0), font=ft)
-    # del draw
-
-def watermark(source, target, watermark, blend_mode='overlay', ratio=1, transparency=0.3):
-    source_obj = WandImage(filename=source)
-    watermark_obj = WandImage(filename=watermark)
-
-    if ratio > 1:
-        ratio = 1
-
-    watermark_width = int(source_obj.size[0] * ratio)
-    watermark_height = int(float(watermark_width) / float(watermark_obj.size[0]) * watermark_obj.size[1])
-
-    rsvg = subprocess.Popen(
-        ['rsvg-convert', '-a', '-h', str(watermark_height), watermark],
-        stdout=subprocess.PIPE
-    )
-
-    #value = rsvg.stdout.read()
-
-    watermark_obj = WandImage(format="svg", blob=rsvg.stdout.read())
-    del rsvg
-
-    watermark_obj.transparentize(transparency)
-    source_obj.composite_channel(
-        'all_channels', watermark_obj, blend_mode,
-        source_obj.size[0] / 2 - watermark_obj.size[0] / 2,
-        source_obj.size[1] / 2 - watermark_obj.size[1] / 2
-    )
-
-    source_obj.save(filename=target)
-
-    return True
