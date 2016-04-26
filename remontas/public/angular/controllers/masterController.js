cea7100643f59bde3fc8863540d894aae4ed22d2
anzolo <alzolotov@gmail.com>
2016-04-26 23:32:17 +0500

    + сервис для получения данных по мастеру
    + выводить аватарку
    + телефоны, общее описание, ФИО, кол-во работ

--- remontas/public/angular/controllers/masterController.js
+++ remontas/public/angular/controllers/masterController.js
@@ -1,17 +1,38 @@
-remontas24Site.controller('masterController', ['$scope', 'masterData', '$stateParams', function ($scope, masterData, $stateParams) {
-    console.log("---> " + $stateParams.id);
+remontas24Site.controller('masterController', ['$scope', 'masterOpenProfile', '$stateParams', function ($scope, masterOpenProfile, $stateParams) {
+
+    $scope.model = {
+        master: undefined
+    };
+
+    $scope.getNameMaster = getNameMaster;
+
     loadData($stateParams.id);
 
     /////////////////////////////////////////////////////////////////////////
 
     function loadData(masterId) {
-        masterData(masterId).init({}, function (data) {
-            if (data.status == "OK") {
-                console.log("Ссервера -> " + data.back);
-            } else if (data.status == "Error") {
-                console.error("Error:", data.note);
-            }
+        masterOpenProfile.get({
+            'masterId': masterId
+        }, function (value, responseHeaders) {
+            $scope.model.master = JSON.parse(JSON.stringify(value.master));
+
+            $scope.model.configUrl = JSON.parse(JSON.stringify(value.configUrl));
+
         });
+    }
+
+    function getNameMaster() {
+        try {
+            if ($scope.model.master.kind_profile == "phys") {
+                return $scope.model.master.sername + " " + $scope.model.master.name + " " + $scope.model.master.patronymic;
+
+            } else if ($scope.model.master.kind_profile == "org") {
+                return $scope.model.master.name
+            } else return ""
+        } catch (ex) {
+            console.error("Error: ", ex.message);
+        }
+
     }
 
 }]);
