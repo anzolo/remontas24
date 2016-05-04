15f7ea61cc2aef92509064ca14a13c9b6e45447d
anzolo <alzolotov@gmail.com>
2016-05-04 15:36:43 +0500

    поправил баги и немного отрефакторил

--- remontas/public/angular/controllers/mainWindow.controller.js
+++ remontas/public/angular/controllers/mainWindow.controller.js
@@ -1,29 +1,55 @@
 remontas24Site.controller('mainController', ['$scope', 'searchMasters', 'ModalService', 'AUTH_EVENTS', 'AuthService', '$state', 'Session', '$rootScope', function ($scope, searchMasters, ModalService, AUTH_EVENTS, AuthService, $state, Session, $rootScope) {
 
-    $scope.isAuth = AuthService.isAuthenticated;
-
     $scope.currentPage = "remontas.searchPage";
     $scope.isAuthOK = false;
+    $scope.showMasteramPodmenu = false;
+    //    $scope.lkButtonName = "Мастерам";
+
+
+    $scope.isAuth = AuthService.isAuthenticated;
+
+    $scope.searchResult = searchMasters.get();
+
+    $scope.setMasteramPodmenuVisible = setMasteramPodmenuVisible;
+    $scope.showAuthRegForm = showAuthRegForm;
+    $scope.logout = logout;
+    $scope.nameOfMasterMenu = nameOfMasterMenu;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////
 
     $rootScope.$on('$stateChangeSuccess',
         function (event, toState, toParams, fromState, fromParams) {
 
             $scope.currentPage = toState.name;
-            //            console.log("$scope.currentPage = ", $scope.currentPage);
 
         })
 
-    $scope.searchResult = searchMasters.get();
+    $scope.$on(AUTH_EVENTS.notAuthenticated, function () {
+        AuthService.logout();
+        $state.go('remontas.searchPage');
+    });
 
-    $scope.showMasteramPodmenu = false;
-    $scope.lkButtonName = "Мастерам";
-    //$scope.currentPage = "main";
+    $scope.$on(AUTH_EVENTS.loginSuccess, function () {
 
-    $scope.setMasteramPodmenuVisible = function (value) {
+        $scope.currentPage = "lk";
+        $scope.isAuthOK = true;
+    });
+
+    function nameOfMasterMenu() {
+        if (AuthService.isAuthenticated()) {
+            return "Личный кабинет";
+        } else {
+            return "Мастерам";
+        }
+    }
+
+
+    function setMasteramPodmenuVisible(value) {
         $scope.showMasteramPodmenu = value;
     };
 
-    $scope.showAuthRegForm = function () {
+    function showAuthRegForm() {
         if (AuthService.isAuthenticated()) {
             //            console.log("Session.token = ", Session.token());
             $state.go('remontas.lk');
@@ -36,20 +62,8 @@ remontas24Site.controller('mainController', ['$scope', 'searchMasters', 'ModalSe
 
     };
 
-    $scope.$on(AUTH_EVENTS.notAuthenticated, function () {
+    function logout() {
         AuthService.logout();
-        $state.go('remontas.searchPage');
-    });
-
-    $scope.$on(AUTH_EVENTS.loginSuccess, function () {
-        $scope.lkButtonName = "Личный кабинет";
-        $scope.currentPage = "lk";
-        $scope.isAuthOK = true;
-    });
-
-    $scope.logout = function () {
-        AuthService.logout();
-        $scope.lkButtonName = "Мастерам";
         $scope.isAuthOK = false;
         $state.go('remontas.searchPage');
     };
