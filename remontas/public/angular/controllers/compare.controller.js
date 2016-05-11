b86ebbeb914176601c3c22b00a6b188085e583d1
Кришталев Александр <AGKrishtalev@gmail.com>
2016-05-12 03:29:19 +0500

    список сравнения данные

--- remontas/public/angular/controllers/compare.controller.js
+++ remontas/public/angular/controllers/compare.controller.js
@@ -1,13 +1,44 @@
-remontas24Site.controller('compareController', ['$scope', 'Session', 'compareService', function ($scope, Session, compareService) {
+remontas24Site.controller('compareController', ['$scope', 'Session', 'compareService', '$sce', function ($scope, Session, compareService, $sce) {
 
     $scope.model = {}
+
+    $scope.showPhone = showPhone;
+    $scope.prepareHTML = prepareHTML;
+    $scope.showPrice = showPrice;
+    $scope.checkJob = checkJob;
+    $scope.checkedJob = checkedJob;
 
     compareService.compare({
         "masters": Session.favourites()
     }, function (data) {
-        $scope.model.configUrl = JSON.parse(JSON.stringify(data.configUrl))
-        $scope.model.masters = JSON.parse(JSON.stringify(data.masters))
-        $scope.model.categories = JSON.parse(JSON.stringify(data.categories))
+        $scope.model.configUrl = JSON.parse(JSON.stringify(data.configUrl));
+        $scope.model.masters = JSON.parse(JSON.stringify(data.masters));
+        $scope.model.categories = JSON.parse(JSON.stringify(data.categories));
+        $scope.model.checked = "";
     })
 
+    function showPhone(phone) {
+        if (phone != "") {
+            return "+7 (" + phone.substr(0, 3) + ") " + phone.substr(3, 3) + " " + phone.substr(6, 2) + " " + phone.substr(8, 2);
+        }
+        return phone;
+    }
+
+    function showPrice(category, index, master) {
+        if (master.prices[category._id] != undefined) return master.prices[category._id] + "<span>" + category.measure + "</span>";
+        return "-----";
+    }
+
+    function prepareHTML(element) {
+        return $sce.trustAsHtml(element);
+    };
+
+    function checkJob(event) {
+        //        console.log(event.target.attributes['data-param'].value)
+        $scope.model.checked = event.target.attributes['data-param'].value;
+    }
+
+    function checkedJob(event) {
+        return $scope.model.checked == event;
+    }
 }]);
