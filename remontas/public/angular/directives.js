2d45f2c6859cb1122961d41b62812b1c95fde4e0
anzolo <alzolotov@gmail.com>
2016-05-17 00:33:25 +0500

    пагинация на главной странице, красивый вывод

--- remontas/public/angular/directives.js
+++ remontas/public/angular/directives.js
@@ -5,14 +5,30 @@ remontas24Site.directive('mastersSearchBox', function() {
             data: '=data'
         },
         link: function(scope, element) {
-            scope.getNumber = function(num) {
-                return new Array(num);
-            };
 
             scope.getIdForMaster = function(master) {
                 if (master.alias_id !== undefined && master.alias_id !== "")
                     return master.alias_id;
                 else return master._id;
+            }
+
+            scope.getRange = function(start, edge, step) {
+                // If only one number was passed in make it the edge and 0 the start.
+                if (arguments.length == 1) {
+                    edge = start;
+                    start = 0;
+                }
+
+                // Validate the edge and step numbers.
+                edge = edge || 0;
+                step = step || 1;
+
+                // Create the array of numbers, stopping befor the edge.
+                for (var ret = [];
+                    (edge - start) * step > 0; start += step) {
+                    ret.push(start);
+                }
+                return ret;
             }
 
         },
