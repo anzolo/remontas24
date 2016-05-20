d9e9798e6035512f1d6c4ac34a87b452ea4c5b11
Кришталев Александр <AGKrishtalev@gmail.com>
2016-05-21 03:49:10 +0500

    Страница Сравнение мастеров. Почти доделано.

--- remontas/public/angular/controllers/compare.controller.js
+++ remontas/public/angular/controllers/compare.controller.js
@@ -11,6 +11,7 @@ remontas24Site.controller('compareController', ['$scope', 'Session', 'compareSer
     $scope.dropMaster = dropMaster;
     $scope.getIdForMaster = getIdForMaster;
     $scope.haveAdditionalService = haveAdditionalService;
+    $scope.showService = showService;
 
     loadData();
 
@@ -23,6 +24,7 @@ remontas24Site.controller('compareController', ['$scope', 'Session', 'compareSer
             $scope.model.categories = JSON.parse(JSON.stringify(data.categories));
             $scope.model.averagePrices = JSON.parse(JSON.stringify(data.averagePrices.prices));
             shrinkServiceText();
+            addToCategoriesParams();
             $scope.model.checked = "";
             $scope.model.mouseOver = "";
 
@@ -37,19 +39,19 @@ remontas24Site.controller('compareController', ['$scope', 'Session', 'compareSer
     function showPhone(phone) {
         if (phone != "") {
             return "+7 (" + phone.substr(0, 3) + ") " + phone.substr(3, 3) + " " + phone.substr(6, 2) + " " + phone.substr(8, 2);
-        }
+        };
         return phone;
-    }
+    };
 
     function showPrice(category, master) {
         if (master.prices[category._id] != undefined) return master.prices[category._id];
         return "-----";
-    }
+    };
 
     function showAveragePrice(category) {
         if ($scope.model.averagePrices[category._id] != undefined) return $scope.model.averagePrices[category._id] + "<span>" + category.measure + "</span>";
         return "";
-    }
+    };
 
     function chackAveragePrice(category, master) {
         if ($scope.model.averagePrices[category._id] != undefined) {
@@ -58,7 +60,7 @@ remontas24Site.controller('compareController', ['$scope', 'Session', 'compareSer
             };
         };
         return false;
-    }
+    };
 
     function prepareHTML(element) {
         return $sce.trustAsHtml(element);
@@ -68,7 +70,7 @@ remontas24Site.controller('compareController', ['$scope', 'Session', 'compareSer
         $scope.model.categories.forEach(function (service, i, arr) {
             if (service.type == 'job') {
                 if (service.name.length > 67) service.shortName = service.name.substring(0, 66) + "..."
-            }
+            };
         });
     };
 
@@ -88,9 +90,24 @@ remontas24Site.controller('compareController', ['$scope', 'Session', 'compareSer
         if (master.alias_id !== undefined && master.alias_id !== "")
             return master.alias_id;
         else return master._id;
-    }
+    };
 
     function haveAdditionalService(master, service) {
         return (master.additional_service.indexOf(service) >= 0) ? "Да" : "Нет";
-    }
+    };
+
+    function addToCategoriesParams() {
+        $scope.model.categories.forEach(function (service, i, arr) {
+            service.visible = true;
+            if (service.type == 'service') service.open = true
+        });
+    };
+
+    function showService(index) {
+        $scope.model.categories[index].open = !$scope.model.categories[index].open;
+        for (var i = index + 1; i < $scope.model.categories.length; i++) {
+            if ($scope.model.categories[i].type == 'service') break
+            $scope.model.categories[i].visible = !$scope.model.categories[i].visible;
+        };
+    };
 }]);
